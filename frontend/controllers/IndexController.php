c67b29b34b3497b524271078c0d65a3426f88c9f
lilong20065 <35793001+lilong20065@users.noreply.github.com>
2018-02-02 00:24:38 +0800

    新增首页

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/2
+ * Time: 0:23
+ */
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+    public function actionIndex(){
+        //
+    }
+}
